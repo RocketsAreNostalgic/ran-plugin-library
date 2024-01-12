b4952e41a9ae8e6bbc41e7493ab8a92f66800580
bnjmnrsh <bnjmnrsh@gmail.com>
2024-01-12 14:02:25 +0000

    refactor action hooks

--- inc/HooksAccessory/ActionHooksManager.php
+++ inc/HooksAccessory/ActionHooksManager.php
@@ -1,4 +1,6 @@
 <?php
+
+declare(strict_types=1);
 /**
  * Manage the implementation of Filter and Action Hooks.
  *
@@ -7,25 +9,26 @@
  *  @package  RanPluginLib
  */
 
-namespace Ran\PluginLib\HooksAsset;
+namespace Ran\PluginLib\HooksAccessory;
 
-use Ran\PluginLib\AssetsAPI\AssetBaseInterface;
-use Ran\PluginLib\AssetsAPI\AssetManagerBaseInterface;
-use Ran\PluginLib\HooksAsset\ActionHooksInterface;
+use Ran\PluginLib\AccessoryAPI\AccessoryBaseInterface;
+use Ran\PluginLib\HooksAccessory\ActionHooksInterface;
 
 /**
  * HooksAPIManager manages object that implement objects that would like to register Wordpress hooks
  * by implementing ActionHookAssetInterface.
  */
-class ActionHooksAssetManager implements AssetManagerBaseInterface {
+class ActionHooksManager implements AccessoryBaseInterface
+{
 	/**
 	 * Registers an object with the WordPress Plugin API.
 	 *
 	 * @param AssetBaseInterface $object An object that implements either the AssetBaseInterface or FilterHookAssetInterface.
 	 */
-	public function init( ActionHooksInterface $object ):void {
-		if ( $object instanceof ActionHooksInterface ) {
-			$this->register_actions( $object );
+	public function init(ActionHooksInterface $object): void
+	{
+		if ($object instanceof ActionHooksInterface) {
+			$this->register_actions($object);
 		}
 	}
 
@@ -36,11 +39,12 @@ class ActionHooksAssetManager implements AssetManagerBaseInterface {
 	 * @param string               $name The name of the action hook.
 	 * @param mixed                $parameters The hook parameters.
 	 */
-	private function register_action( ActionHooksInterface $object, $name, $parameters ) {
-		if ( is_string( $parameters ) ) {
-			add_action( $name, array( $object, $parameters ) );
-		} elseif ( is_array( $parameters ) && isset( $parameters[0] ) ) {
-			add_action( $name, array( $object, $parameters[0] ), isset( $parameters[1] ) ? $parameters[1] : 10, isset( $parameters[2] ) ? $parameters[2] : 1 );
+	private function register_action(ActionHooksInterface $object, $name, $parameters)
+	{
+		if (is_string($parameters)) {
+			add_action($name, array($object, $parameters));
+		} elseif (is_array($parameters) && isset($parameters[0])) {
+			add_action($name, array($object, $parameters[0]), isset($parameters[1]) ? $parameters[1] : 10, isset($parameters[2]) ? $parameters[2] : 1);
 		}
 	}
 
@@ -49,9 +53,10 @@ class ActionHooksAssetManager implements AssetManagerBaseInterface {
 	 *
 	 * @param ActionHooksInterface $object Any object the implements the ActionHookAssetInterface.
 	 */
-	private function register_actions( ActionHooksInterface $object ) {
-		foreach ( $object->get_actions() as $name => $parameters ) {
-			$this->register_action( $object, $name, $parameters );
+	private function register_actions(ActionHooksInterface $object)
+	{
+		foreach ($object->get_actions() as $name => $parameters) {
+			$this->register_action($object, $name, $parameters);
 		}
 	}
 }
