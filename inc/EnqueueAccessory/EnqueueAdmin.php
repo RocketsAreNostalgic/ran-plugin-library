520089181db1ee6c895830e6ecd7bf247fd03ad7
bnjmnrsh <bnjmnrsh@gmail.com>
2024-01-15 01:57:37 +0000

    formatting, docbloc

--- inc/EnqueueAccessory/EnqueueAdmin.php
+++ inc/EnqueueAccessory/EnqueueAdmin.php
@@ -1,10 +1,11 @@
 <?php
-
-declare(strict_types=1);
 /**
+ * This class is meant to be implemented and instantiated via the RegisterServices Class.
+ *
  * @package  RanPluginLib
  */
 
+declare(strict_types=1);
 namespace Ran\PluginLib\EnqueueAccessory;
 
 /**
@@ -12,8 +13,8 @@ namespace Ran\PluginLib\EnqueueAccessory;
  *
  * @package  RanPluginLib
  */
-final class EnqueueAdmin extends EnqueueAbstract implements EnqueueInterface
-{
+final class EnqueueAdmin extends EnqueueAbstract implements EnqueueInterface {
+
 
 	/**
 	 * A class registration function to add the wp_enqueue_scripts hook to WP.
@@ -21,8 +22,7 @@ final class EnqueueAdmin extends EnqueueAbstract implements EnqueueInterface
 	 *
 	 * @return void
 	 */
-	public function load(): void
-	{
-		add_action('admin_enqueue_scripts', array($this, 'enqueue'));
+	public function load(): void {
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
 	}
 }
