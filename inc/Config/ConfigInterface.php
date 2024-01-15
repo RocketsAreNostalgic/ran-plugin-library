1a1883112b330e67b5610ca704d74d5f892f6dc7
bnjmnrsh <bnjmnrsh@gmail.com>
2024-01-15 01:56:24 +0000

    Remove refs to Plugin Class, add get_plugin()

--- inc/Config/ConfigInterface.php
+++ inc/Config/ConfigInterface.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * An interface for the Config class.
  *
@@ -11,12 +10,11 @@ declare(strict_types=1);
 namespace Ran\PluginLib\Config;
 
 /**
- * Interface for the Plugin class which holds key information about the plugin.
+ * Interface for the Config class which holds key information about the plugin.
  *
  * @package  RanPluginLib
  */
 interface ConfigInterface {
-
 	/**
 	 * Returns the value of an active option, or false.
 	 *
@@ -26,5 +24,12 @@ interface ConfigInterface {
 	 *
 	 * @return array|string|false the value of the option key (string or array), or false
 	 */
-	public function get_plugin_options( string $key, string $option_string = ''): mixed;
+	public function get_plugin_options( string $key, string $option_string = '' ): mixed;
+
+	/**
+	 * Returns an array of plugin config properties.
+	 *
+	 * @return array config array
+	 */
+	public function get_plugin(): array;
 }
