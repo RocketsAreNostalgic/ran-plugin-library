69c89c8c4611b40de2cde483df9b7218603f3d09
bnjmnrsh <bnjmnrsh@gmail.com>
2024-01-21 22:47:26 +0000

    Add singleton base class to extend.

---
+++ inc/Singleton/Singleton.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * A convenience Singleton final class.
+ *
+ *  @package  RanPluginLib
+ */
+
+declare(strict_types=1);
+namespace Ran\PluginLib\Singleton;
+
+/**
+ * Singleton convenience class.
+ */
+class Singleton extends SingletonAbstract {
+	// Go extend your own.
+}
