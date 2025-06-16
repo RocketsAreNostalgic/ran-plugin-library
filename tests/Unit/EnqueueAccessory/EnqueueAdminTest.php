746fdf197fe0d221ce86acb070d97b3cf3f0a22f
Benjamin Rush <bnjmnrsh@gmail.com>
2025-06-16 20:43:21 +0100

    fix(StylesEnqueueTrait): Ensure accurate style count logging and update tests
    - Modified `add_styles` in `StylesEnqueueTrait.php` to normalize single style definitions to an array format at the beginning of the method. This ensures that the initial log message accurately reflects the number of styles being processed.
    - Updated logger mock expectations in `StylesEnqueueTraitTest.php` for the tests `test_enqueue_styles_deferred_style_missing_handle_logs_na_and_stores` and `test_add_styles_with_single_non_nested_style`.
    - The expectations now correctly anticipate logs for a single style addition, including accurate details for the style's key, handle, and source path in the "Adding style" log message.
    - These changes correct previous test failures that were due to discrepancies between the number of styles logged and the number expected by the mocks.

--- tests/Unit/EnqueueAccessory/EnqueueAdminTest.php
+++ tests/Unit/EnqueueAccessory/EnqueueAdminTest.php
@@ -43,7 +43,7 @@ class EnqueueAdminTest extends TestCase {
 		$this->config_mock->shouldReceive( 'get_logger' )->andReturn( $this->logger_mock );
 
 		// Use a real SUT to test the integration of its methods.
-		$this->sut = new EnqueueAdmin( $this->config_mock );
+		$this->sut        = new EnqueueAdmin( $this->config_mock );
 		$this->reflection = new \ReflectionClass( EnqueueAdmin::class );
 
 		// Default logger to inactive. Tests that need logging should explicitly enable it.
@@ -65,7 +65,7 @@ class EnqueueAdminTest extends TestCase {
 			$class = $class->getParentClass();
 		}
 		if ( ! $class ) {
-			throw new \ReflectionException( "Property {$name} does not exist in class " . $this->reflection->getName() . " or its parents." );
+			throw new \ReflectionException( "Property {$name} does not exist in class " . $this->reflection->getName() . ' or its parents.' );
 		}
 		$property = $class->getProperty( $name );
 		$property->setAccessible( true );
@@ -76,15 +76,15 @@ class EnqueueAdminTest extends TestCase {
 		WP_Mock::userFunction( 'is_admin' )->andReturn( false );
 
 		// With the guard clause, no other functions should be called.
-		WP_Mock::expectActionNotAdded( 'admin_enqueue_scripts', [ $this->sut, 'enqueue' ] );
-		WP_Mock::expectActionNotAdded( 'admin_head', [ $this->sut, 'render_head' ] );
-		WP_Mock::expectActionNotAdded( 'admin_footer', [ $this->sut, 'render_footer' ] );
+		WP_Mock::expectActionNotAdded( 'admin_enqueue_scripts', array( $this->sut, 'enqueue' ) );
+		WP_Mock::expectActionNotAdded( 'admin_head', array( $this->sut, 'render_head' ) );
+		WP_Mock::expectActionNotAdded( 'admin_footer', array( $this->sut, 'render_footer' ) );
 
 		$this->sut->load();
 		$this->assertConditionsMet();
 	}
 
-	public function test_load_does_nothing_if_not_admin_with_logging() {
+	public function test_skip_load_does_nothing_if_not_admin_with_logging() {
 		$this->logger_mock->shouldReceive( 'is_active' )->andReturn( true );
 		$this->logger_mock->shouldReceive( 'debug' )->with( 'EnqueueAdmin::load() - Method entered.' )->once();
 		$this->logger_mock->shouldReceive( 'debug' )->with( 'EnqueueAdmin::load() - Not an admin request. Bailing.' )->once();
@@ -101,7 +101,7 @@ class EnqueueAdminTest extends TestCase {
 		WP_Mock::userFunction( 'is_admin' )->andReturn( true );
 		WP_Mock::userFunction( 'did_action' )->with( 'admin_enqueue_scripts' )->andReturn( false );
 
-		WP_Mock::expectActionAdded( 'admin_enqueue_scripts', [ $this->sut, 'enqueue' ] );
+		WP_Mock::expectActionAdded( 'admin_enqueue_scripts', array( $this->sut, 'enqueue' ) );
 
 		$this->sut->load();
 		$this->assertConditionsMet();
@@ -122,21 +122,22 @@ class EnqueueAdminTest extends TestCase {
 		WP_Mock::userFunction( 'wp_add_inline_style' )->andReturnNull()->zeroOrMoreTimes();
 		WP_Mock::userFunction( 'wp_add_inline_script' )->andReturnNull()->zeroOrMoreTimes();
 
-		WP_Mock::expectActionNotAdded( 'admin_enqueue_scripts', [ $this->sut, 'enqueue' ] );
+		WP_Mock::expectActionNotAdded( 'admin_enqueue_scripts', array( $this->sut, 'enqueue' ) );
 
 		$this->sut->load();
 		$this->assertConditionsMet();
 	}
 
 	public function test_load_hooks_render_head_when_callbacks_exist_and_hook_not_fired() {
-		$this->set_protected_property( 'head_callbacks', [ function() {} ] );
+		$this->set_protected_property( 'head_callbacks', array( function() {
+		} ) );
 
 		WP_Mock::userFunction( 'is_admin' )->andReturn( true );
 		WP_Mock::userFunction( 'did_action' )->with( 'admin_enqueue_scripts' )->andReturn( false );
 		WP_Mock::userFunction( 'did_action' )->with( 'admin_head' )->andReturn( false );
 
-		WP_Mock::expectActionAdded( 'admin_enqueue_scripts', [ $this->sut, 'enqueue' ] );
-		WP_Mock::expectActionAdded( 'admin_head', [ $this->sut, 'render_head' ] );
+		WP_Mock::expectActionAdded( 'admin_enqueue_scripts', array( $this->sut, 'enqueue' ) );
+		WP_Mock::expectActionAdded( 'admin_head', array( $this->sut, 'render_head' ) );
 
 		$this->sut->load();
 		$this->assertConditionsMet();
@@ -144,8 +145,10 @@ class EnqueueAdminTest extends TestCase {
 
 	public function test_load_calls_render_head_directly_when_callbacks_exist_and_hook_fired() {
 		// Use a real closure to avoid issues with `is_callable`.
-		$callback = function() { echo "test"; };
-		$this->set_protected_property( 'head_callbacks', [ $callback ] );
+		$callback = function() {
+			echo 'test';
+		};
+		$this->set_protected_property( 'head_callbacks', array( $callback ) );
 
 		WP_Mock::userFunction( 'is_admin' )->andReturn( true );
 		WP_Mock::userFunction( 'did_action' )->with( 'admin_enqueue_scripts' )->andReturn( true );
@@ -161,7 +164,7 @@ class EnqueueAdminTest extends TestCase {
 		// No need to mock `is_callable` or `call_user_func` as they are internal PHP functions.
 		// WP_Mock cannot and should not mock them. Providing a real callable is the correct approach.
 
-		WP_Mock::expectActionNotAdded( 'admin_head', [ $this->sut, 'render_head' ] );
+		WP_Mock::expectActionNotAdded( 'admin_head', array( $this->sut, 'render_head' ) );
 
 		// Expect the callback to be executed.
 		$this->expectOutputString( 'test' );
@@ -171,22 +174,25 @@ class EnqueueAdminTest extends TestCase {
 	}
 
 	public function test_load_hooks_render_footer_when_callbacks_exist_and_hook_not_fired() {
-		$this->set_protected_property( 'footer_callbacks', [ function() {} ] );
+		$this->set_protected_property( 'footer_callbacks', array( function() {
+		} ) );
 
 		WP_Mock::userFunction( 'is_admin' )->andReturn( true );
 		WP_Mock::userFunction( 'did_action' )->with( 'admin_enqueue_scripts' )->andReturn( false );
 		WP_Mock::userFunction( 'did_action' )->with( 'admin_footer' )->andReturn( false );
 
-		WP_Mock::expectActionAdded( 'admin_enqueue_scripts', [ $this->sut, 'enqueue' ] );
-		WP_Mock::expectActionAdded( 'admin_footer', [ $this->sut, 'render_footer' ] );
+		WP_Mock::expectActionAdded( 'admin_enqueue_scripts', array( $this->sut, 'enqueue' ) );
+		WP_Mock::expectActionAdded( 'admin_footer', array( $this->sut, 'render_footer' ) );
 
 		$this->sut->load();
 		$this->assertConditionsMet();
 	}
 
 	public function test_load_calls_render_footer_directly_when_callbacks_exist_and_hook_fired() {
-		$callback = function() { echo "test_footer"; };
-		$this->set_protected_property( 'footer_callbacks', [ $callback ] );
+		$callback = function() {
+			echo 'test_footer';
+		};
+		$this->set_protected_property( 'footer_callbacks', array( $callback ) );
 
 		WP_Mock::userFunction( 'is_admin' )->andReturn( true );
 		WP_Mock::userFunction( 'did_action' )->with( 'admin_enqueue_scripts' )->andReturn( true );
@@ -198,7 +204,7 @@ class EnqueueAdminTest extends TestCase {
 		WP_Mock::userFunction( 'wp_enqueue_style' )->andReturnNull()->zeroOrMoreTimes();
 		WP_Mock::userFunction( 'wp_enqueue_script' )->andReturnNull()->zeroOrMoreTimes();
 
-		WP_Mock::expectActionNotAdded( 'admin_footer', [ $this->sut, 'render_footer' ] );
+		WP_Mock::expectActionNotAdded( 'admin_footer', array( $this->sut, 'render_footer' ) );
 
 		$this->expectOutputString( 'test_footer' );
 
@@ -208,8 +214,10 @@ class EnqueueAdminTest extends TestCase {
 
 	public function test_load_with_logging_hooks_all_actions_when_hooks_not_fired() {
 		// Setup: Callbacks exist, but no hooks have fired yet.
-		$this->set_protected_property( 'head_callbacks', [ function() {} ] );
-		$this->set_protected_property( 'footer_callbacks', [ function() {} ] );
+		$this->set_protected_property( 'head_callbacks', array( function() {
+		} ) );
+		$this->set_protected_property( 'footer_callbacks', array( function() {
+		} ) );
 
 		WP_Mock::userFunction( 'is_admin' )->andReturn( true );
 		WP_Mock::userFunction( 'did_action' )->with( 'admin_enqueue_scripts' )->andReturn( false );
@@ -228,9 +236,9 @@ class EnqueueAdminTest extends TestCase {
 		$this->logger_mock->shouldReceive( 'debug' )->with( 'EnqueueAdmin::load() - Method exited.' )->once()->ordered();
 
 		// Expectations: Actions are added.
-		WP_Mock::expectActionAdded( 'admin_enqueue_scripts', [ $this->sut, 'enqueue' ] );
-		WP_Mock::expectActionAdded( 'admin_head', [ $this->sut, 'render_head' ] );
-		WP_Mock::expectActionAdded( 'admin_footer', [ $this->sut, 'render_footer' ] );
+		WP_Mock::expectActionAdded( 'admin_enqueue_scripts', array( $this->sut, 'enqueue' ) );
+		WP_Mock::expectActionAdded( 'admin_head', array( $this->sut, 'render_head' ) );
+		WP_Mock::expectActionAdded( 'admin_footer', array( $this->sut, 'render_footer' ) );
 
 		// Execute
 		$this->sut->load();
@@ -243,29 +251,94 @@ class EnqueueAdminTest extends TestCase {
 	public function test_load_with_logging_calls_all_methods_directly_when_hooks_fired() {
 		// Override logger mock from setUp, allowing multiple calls.
 		$this->logger_mock->shouldReceive( 'is_active' )->andReturn( true )->zeroOrMoreTimes();
-		$this->logger_mock->shouldReceive('debug')->with('EnqueueAdmin::load() - Method entered.')->once();
-		$this->logger_mock->shouldReceive('debug')->with('EnqueueAdmin::load() - admin_enqueue_scripts already fired. Calling enqueue() directly.')->once();
-		$this->logger_mock->shouldReceive('debug')->with('EnqueueAdmin::load() - Checking for head callbacks. Count: 1')->once();
-		$this->logger_mock->shouldReceive('debug')->with('EnqueueAdmin::load() - admin_head already fired. Calling render_head() directly.')->once();
-		$this->logger_mock->shouldReceive('debug')->with('EnqueueAdmin::load() - admin_footer already fired. Calling render_footer() directly.')->once();
-		// Add a permissive expectation for any other debug messages.
-		$this->logger_mock->shouldReceive('debug')->withAnyArgs()->zeroOrMoreTimes();
 
-		$this->set_protected_property('head_callbacks', ['test']);
-		$this->set_protected_property('footer_callbacks', ['test']);
+		// Local helper to robustly set protected properties on any object.
+		$setProperty = function($object, $property, $value) {
+			$reflection = new \ReflectionClass($object);
+			$class_to_reflect = $reflection;
+			while ($class_to_reflect && !$class_to_reflect->hasProperty($property)) {
+				$class_to_reflect = $class_to_reflect->getParentClass();
+			}
+			if (!$class_to_reflect) {
+				throw new \ReflectionException("Property {$property} does not exist in class " . $reflection->getName() . ' or its parents.');
+			}
+			$prop = $class_to_reflect->getProperty($property);
+			$prop->setAccessible(true);
+			$prop->setValue($object, $value);
+		};
 
-		WP_Mock::userFunction( 'is_admin' )->andReturn( true );
-		WP_Mock::userFunction( 'did_action' )->with( 'admin_enqueue_scripts' )->andReturn( true );
-		WP_Mock::userFunction( 'did_action' )->with( 'admin_head' )->andReturn( true );
-		WP_Mock::userFunction( 'did_action' )->with( 'admin_footer' )->andReturn( true );
+		// --- CONFIGURE THE SUT'S STATE VIA THE CONSTRUCTOR ---
+		// The SUT's logger is injected via the config mock in the constructor.
+		$this->config_mock->shouldReceive('get')->with('enqueue.admin.scripts', Mockery::any())->andReturn([]);
+		$this->config_mock->shouldReceive('get')->with('enqueue.admin.styles', Mockery::any())->andReturn([]);
 
-		// Since we are using a real object, we need to mock the methods called by enqueue(), etc.
-		WP_Mock::userFunction( 'wp_enqueue_style' )->andReturnNull()->zeroOrMoreTimes();
-		WP_Mock::userFunction( 'wp_enqueue_script' )->andReturnNull()->zeroOrMoreTimes();
-		WP_Mock::userFunction( 'wp_add_inline_style' )->andReturnNull()->zeroOrMoreTimes();
-		WP_Mock::userFunction( 'wp_add_inline_script' )->andReturnNull()->zeroOrMoreTimes();
+		// Re-instantiate the SUT with the configured mock.
+		$sut = $this->getMockBuilder(EnqueueAdmin::class)
+			->setConstructorArgs([$this->config_mock])
+			->onlyMethods(['render_head', 'render_footer'])
+			->getMock();
 
-		$this->sut->load();
+		// Force the script and style arrays to be empty to ensure a clean state.
+		$setProperty($sut, 'scripts', []);
+		$setProperty($sut, 'styles', []);
+
+		// --- Logger Expectations ---
+		// 1. Load method entered
+		$this->logger_mock->shouldReceive('debug')->with('EnqueueAdmin::load() - Method entered.')->once()->ordered();
+
+		// 2. Enqueue hook already fired, calling enqueue() directly
+		$this->logger_mock->shouldReceive('debug')->with('EnqueueAdmin::load() - admin_enqueue_scripts already fired. Calling enqueue() directly.')->once()->ordered();
+
+		// 3. Main enqueue process started
+		$this->logger_mock->shouldReceive('debug')->with('AssetEnqueueBaseAbstract::enqueue - Main enqueue process started. Scripts: 0, Styles: 0, Media: 0, Inline Scripts: 0.')->once()->ordered();
+
+		// 4. Asset processing (from traits, called by enqueue())
+		// Scripts
+		$this->logger_mock->shouldReceive('debug')->with('ScriptsEnqueueTrait::enqueue_scripts - Entered. Processing 0 script definition(s).')->once()->ordered();
+		$this->logger_mock->shouldReceive('debug')->with('ScriptsEnqueueTrait::enqueue_scripts - hooks_with_new_scripts: []')->once()->ordered();
+		$this->logger_mock->shouldReceive('debug')->with('ScriptsEnqueueTrait::enqueue_scripts - Exited.')->once()->ordered();
+
+		// Styles
+		$this->logger_mock->shouldReceive('debug')->with('StylesEnqueueTrait::enqueue_styles - Entered. Processing 0 style definition(s) from internal queue.')->once()->ordered();
+		$this->logger_mock->shouldReceive('debug')->with('StylesEnqueueTrait::enqueue_styles - Exited. Deferred styles count: 0.')->once()->ordered();
+
+		// Media
+		$this->logger_mock->shouldReceive('debug')->with('MediaEnqueueTrait::enqueue_media - Entered. Processing 0 media tool configuration(s).')->once()->ordered();
+		$this->logger_mock->shouldReceive('debug')->with('MediaEnqueueTrait::enqueue_media - Exited.')->once()->ordered();
+
+		// Inline Scripts
+		$this->logger_mock->shouldReceive('debug')->with('ScriptsEnqueueTrait::enqueue_inline_scripts - Entered method.')->once()->ordered();
+
+		// 5. Main enqueue process finished
+		$this->logger_mock->shouldReceive('debug')->with('AssetEnqueueBaseAbstract::enqueue - Main enqueue process finished.')->once()->ordered();
+
+		// 6. Head callbacks
+		$this->logger_mock->shouldReceive('debug')->with('EnqueueAdmin::load() - Checking for head callbacks. Count: 1')->once()->ordered();
+		$this->logger_mock->shouldReceive('debug')->with('EnqueueAdmin::load() - admin_head already fired. Calling render_head() directly.')->once()->ordered();
+
+		// 7. Footer callbacks
+		$this->logger_mock->shouldReceive('debug')->with('EnqueueAdmin::load() - Checking for footer callbacks. Count: 1')->once()->ordered();
+		$this->logger_mock->shouldReceive('debug')->with('EnqueueAdmin::load() - admin_footer already fired. Calling render_footer() directly.')->once()->ordered();
+
+		// 8. Load method exited
+		$this->logger_mock->shouldReceive('debug')->with('EnqueueAdmin::load() - Method exited.')->once()->ordered();
+
+		// Set protected properties on the NEW SUT
+		$setProperty($sut, 'head_callbacks', ['test']);
+		$setProperty($sut, 'footer_callbacks', ['test']);
+
+		// Mock WordPress hook status
+		\WP_Mock::userFunction( 'is_admin' )->andReturn( true );
+		\WP_Mock::userFunction( 'did_action' )->with( 'admin_enqueue_scripts' )->andReturn( true );
+		\WP_Mock::userFunction( 'did_action' )->with( 'admin_head' )->andReturn( true );
+		\WP_Mock::userFunction( 'did_action' )->with( 'admin_footer' )->andReturn( true );
+
+		// Mock the render methods on our new SUT
+		$sut->expects($this->once())->method('render_head');
+		$sut->expects($this->once())->method('render_footer');
+
+		// Execute
+		$sut->load();
 		$this->assertConditionsMet();
 	}
 }
